f9b69f350220d2c4202386b700eeef048b1398a3
Min Htet Ko <penguincrab99@gmail.com>
2023-07-04 10:43:42 +0630

    Updated User Model and Testing create user api

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Models\User;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -16,4 +17,13 @@ use Illuminate\Support\Facades\Route;
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
+});
+
+Route::get('users/create', function () {
+    $user = User::create([
+        'name' => 'Min Htet Ko',
+        'email' => "mhk@gmail.com",
+        "password" => "12345678"
+    ]);
+    return response()->json(['message' => 'User account created.', 'data' => $user]);
 });
